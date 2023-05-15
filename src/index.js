03da3c703bb9b07d5260fa3d3191dbe6d3a832ca
Adrian Nastał <81535800+Bazky@users.noreply.github.com>
2023-05-15 14:51:20 +0200

    5

--- src/index.js
+++ src/index.js
@@ -41,7 +41,6 @@
 //   console.log('imageProperties');
 // });
 
-import './styles.css';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
