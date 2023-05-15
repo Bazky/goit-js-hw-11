eee108d5e0bf334ccedff449a8084109229bf012
Adrian Nastał <81535800+Bazky@users.noreply.github.com>
2023-05-15 15:33:26 +0200

    7

--- src/index.js
+++ src/index.js
@@ -53,8 +53,29 @@ const URL = `https://pixabay.com/api/?key=${API_KEY}&image_type=photo&orientatio
 
 let page = 1;
 let searchQuery = '';
-
 const lightbox = new SimpleLightbox('.gallery a');
+
+async function fetchImages(searchQuery) {
+  try {
+    const response = await axios.get(URL, {
+      params: {
+        key: API_KEY,
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: page,
+      },
+    });
+
+    return response.data.hits;
+  } catch (error) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+}
 
 searchForm.addEventListener('submit', onSearch);
 loadMoreBtn.addEventListener('click', onLoadMore);
@@ -63,39 +84,19 @@ function onSearch(event) {
   event.preventDefault();
   clearGallery();
   searchQuery = event.currentTarget.elements.searchQuery.value.trim();
-  if (searchQuery === '') {
-    return Notiflix.Notify.warning('Enter something!');
-  }
   page = 1;
   fetchImages().then(renderGallery).catch(console.log);
 }
 
 function onLoadMore() {
   page += 1;
-  fetchImages().then(renderGallery).catch(console.log);
-}
-
-function fetchImages() {
-  const url = `${URL}&q=${searchQuery}&page=${page}`;
-  return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error('Something went wrong!');
-    })
-    .then(({ hits, totalHits }) => {
-      if (hits.length === 0) {
-        Notiflix.Notify.warning(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        loadMoreBtn.classList.add('is-hidden');
-      } else if (hits.length > 0) {
-        Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-        loadMoreBtn.classList.remove('is-hidden');
-      }
-      return hits;
-    });
+  fetchImages()
+    .then(renderGallery)
+    .catch(
+      Notiflix.Notify(
+        "We're sorry, but you've reached the end of search results."
+      )
+    );
 }
 
 function renderGallery(images) {
