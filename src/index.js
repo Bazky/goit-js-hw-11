8de3f1b92e2e95f92849cd36963662880abe7e4c
Adrian Nastał <81535800+Bazky@users.noreply.github.com>
2023-05-20 14:12:09 +0200

    2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './styles.css';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import axios from 'axios';
 
 const searchForm = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
